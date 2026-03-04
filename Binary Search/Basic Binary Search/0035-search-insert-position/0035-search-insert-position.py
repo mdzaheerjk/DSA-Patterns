01b41bd8b443400457300c6a9b04809a59b4336d
zaheer <info.zaheerjk@gmail.com>
2026-03-05 01:56:04 +0530

    binary search

---
+++ Binary Search/Basic Binary Search/0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        first=0
+        last=len(nums)-1
+        while first<=last:
+            mid=(last+first)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                last=mid-1
+            else:
+                first=mid+1
+        return last+1
+        
